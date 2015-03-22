c29f894f74a7105aa8292a48ae16c9a6ae1d411e
SzeChan <chansd5@gmail.com>
2015-03-22 16:41:11 -0400

    React ready, Raw Rails template

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,9 @@ Rails.application.configure do
   # when problems arise.
   config.log_level = :debug
 
+  # React production variant (minified)
+  config.react.variant = :production
+
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,49 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+p = Post.new(title: 'idolatry of uniqueness that afflicts college students and addicts', author: 'David Foster Wallace')
+p.contents = <<EOF
+People of a certain age and level of like life-experience believe they're immortal: 
+college students and alcoholics/addicts are the worst: they deep-down believe they're 
+exempt from the laws of physics and statistics that ironly govern everybody else. 
+They'll piss and moan your ear off if somebody else fucks with the rules, but they 
+don't deep down see themselves subject to them, the same rules. And they're 
+constitutionally unable to learn from anybody else's experience: if some jaywalking 
+B.U. student does get his car towed, your other student's or addict's response to this 
+will be to ponder just what imponderable difference makes it possible for that other 
+guy to get splattered or towed and not him, the ponderer. They never doubt the 
+difference — they just ponder it. It's like a kind of idolatry of uniqueness.
+EOF
+
+p.save
+
+
+p = Post.new(title: 'I am Complex', author: 'Tennis Boy')
+p.contents = <<EOF
+'I read,' I say. 'I study and read. I bet I've read everything you've read. 
+Don't think I haven't. I consume libraries. I wear out spines and ROM-drives. 
+I do things like get in a taxi and say, "The library, and step on it." 
+My instincts concerning syntax and mechanics are better than your own, 
+I can tell, with due respect. But it transcends the mechanics. I'm not a machine. 
+I feel and believe. I have opinions. Some of them are interesting. 
+I could, if you'd let me, talk and talk. Let's talk about anything. 
+I believe the influence of Kierkegaard on Camus is underestimated. 
+I believe Dennis Gabor may very well have been the Antichrist. 
+I believe Hobbes is just Rousseau in a dark mirror. 
+I believe, with Hegel, that transcendence is absorption. 
+I could interface you guys right under the table,' 
+I say. 'I'm not just a creatus, manufactured, conditioned, bred for a function.'
+EOF
+
+p.save
+
+p = Post.new(title: 'Fear of not furfilling potential', author: 'David Foster Wallace')
+p.contents = <<EOF
+Talent is its own expectation, Jim: you either live up to it or it waves a hankie, 
+receding forever. Use it or lose it, he say over the newspaper. I'm…I'm just afraid of 
+having a tombstone that says HERE LIES A PROMISING OLD MAN. Potential maybe worse than 
+none, Jim. Than no talent to fritter in the first place, lying around guzzling because 
+I haven't the balls to…God I'm I'm so sorry Jim. You don't deserve to see me like this. 
+I'm so scared, Jim. I'm so scared of dying without ever really being seen. 
+Can you understand? Are you enough of a big thin prematurely stooped young bespectacled man,
+even with your whole life still ahead of you, to understand? Can you see I was giving it all I had?
+EOF
+
+p.save
